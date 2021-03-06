83a8f557f69519a498ae05e5d00fd131dd928382
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-05 18:21:39 -0600

    Login

---
+++ src/actions/authActions.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { LOGIN_ENDPOINT, REGISTER_ENDPOINT } from '../helpers/endpoints';
+import { types } from './types';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../helpers/setAuthToken';
+
+/* Login*/
+export const loginUser = (userData) => (dispatch) => {   
+	return new Promise((resolve, reject) => {
+		axios
+			.post(LOGIN_ENDPOINT, userData, {
+				headers: { Accept: 'application/json', 'Content-type': 'application/json' },
+			})
+			.then((response) => {//sucess
+                //get token from header and save in localStorage
+				const { authorization } = response.headers;
+				localStorage.setItem('jwtToken', authorization);
+
+				setAuthToken(authorization);
+
+				//decode token 
+				const decoded = jwt_decode(authorization);
+				//token 		
+				dispatch(login({ user: decoded, loggedIn: true }));
+
+				resolve(response); //return response
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
+
+
+export const login = ({ user, loggedIn }) => {
+    return {
+        type: type.authLogin,
+        payload: { user, loggedIn }
+    };
+}
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, Alert } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import SignInForm from '../components/forms/SignInForm';
+import validator from 'validator';
 
-const SignIn = () => {
+
+export default function SignIn() {
+   
+    //detect if a object is empty
+    const isObjEmpty = (obj) => {
+        return Object.keys(obj).length === 0 && obj.constructor === Object;
+    }
+    const [errors, setErrors] = useState({});
+    const dispatch = useDispatch();
+    const loggedIn = useSelector(state => state.auth.loggedIn);
+
+    useEffect(() => {
+      if (loggedIn) {
+        history.push("/");
+      }
+    }); 
+
+    const login = ({ email, password }) => {
+        const errors = {};
+        setErrors(errors);
+
+        if (!validator.isEmail(email)) {
+            errors.email = "email is invalid";
+        }
+
+        if (validator.isEmpty(password)) {
+            errors.password = "password must not empty";
+        }
+
+        if (!isObjEmpty(errors)) {//if there are errors save the errors
+            setErrors(errors); 
+            return;
+        }
+
+        dispatch(loginUser({ email, password }))
+        .then(response => {
+
+        })
+        .catch(err => {                
+            setErrors({ auth: "No se puede iniciar sesion con esos credenciales" });
+        });
+       
+    }
+
     return (
-        <div>
-            Sigin
-        </div>
+        <Container className="mt-5">
+            <Row>
+                <Col sm="12" md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+                    <Card body>                        
+                        { errors.auth && <Alert variant="danger">{ errors.auth }</Alert> }
+
+                        <h3>Log in</h3><hr></hr>
+                        <SignInForm errors={errors} onSubmitLogin={login}></SignInForm>
+                        <div className="mt-4">
+                            <Link to={"/signup"}>Create a new account here.</Link>
+                        </div>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
-
-export default SignIn
-
